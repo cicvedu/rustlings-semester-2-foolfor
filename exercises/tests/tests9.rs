a8901547637ab47957d4fb471a59336c755a21a4
Foolfor <64899331+foolfor@users.noreply.github.com>
2023-11-03 10:20:19 +0000

    update

--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -35,12 +35,8 @@ extern "Rust" {
 
 mod Foo {
     // No `extern` equals `extern "Rust"`.
-    pub fn my_demo_function(a: u32) -> u32 {
-        a
-    }
-    pub fn my_demo_function_alias(a: u32) -> u32 {
-        a
-    }
+    pub fn my_demo_function(a: u32) -> u32 { a }
+    pub fn my_demo_function_alias(a: u32) -> u32 { a }
 }
 
 #[cfg(test)]
